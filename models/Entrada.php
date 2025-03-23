e957db184f694cdeaa892d4a2893f53b39fabdcb
Eileen Lidueñez <eileenlc0509@gmail.com>
2025-03-22 20:38:14 -0500

    cambios x

--- models/Entrada.php
+++ models/Entrada.php
@@ -13,5 +13,15 @@ class Entrada {
         $result = $this->db->query($sql);
         return $result;
     }
+
+    public function crearEntrada($titulo, $contenido, $categoria_id, $usuario_id) {
+        $sql = "INSERT INTO entradas (titulo, contenido, categoria_id, usuario_id) VALUES (?, ?, ?, ?)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bind_param("ssii", $titulo, $contenido, $categoria_id, $usuario_id);
+        return $stmt->execute();
+    }
+
+
+    
 }
 ?>
